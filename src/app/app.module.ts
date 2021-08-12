1c7e94f86d8bd62f5e5036ffedd349d6fbe6f04e
Waylon <wayloncn@gmail.com>
2021-08-12 13:50:19 -0400

    - created JobListingsComponent - created JobListingComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { JobListingsComponent } from './components/job-listings/job-listings.component';
+import { JobListingComponent } from './components/job-listings/job-listing/job-listing.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, LayoutComponent, HeaderComponent],
+  declarations: [AppComponent, HomePageComponent, LayoutComponent, HeaderComponent, JobListingsComponent, JobListingComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
