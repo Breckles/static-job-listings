cb9c50ea7f413968adaa42c8ef02632a23e6937f
Waylon <wayloncn@gmail.com>
2021-08-17 15:11:09 -0400

    wrote test for HighLightPillComponent

--- src/app/components/ui/highlight-pill/highlight-pill.component.spec.ts
+++ src/app/components/ui/highlight-pill/highlight-pill.component.spec.ts
@@ -1,25 +1,36 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HighlightPillComponent } from './highlight-pill.component';
 
+@Component({
+  template: '<app-highlight-pill>Test Successful</app-highlight-pill>',
+})
+class ContentProjectionTestingComponent {}
+
 describe('HighlightPillComponent', () => {
-  let component: HighlightPillComponent;
-  let fixture: ComponentFixture<HighlightPillComponent>;
+  let component: ContentProjectionTestingComponent;
+  let fixture: ComponentFixture<ContentProjectionTestingComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HighlightPillComponent ]
-    })
-    .compileComponents();
+      declarations: [ContentProjectionTestingComponent, HighlightPillComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HighlightPillComponent);
+    fixture = TestBed.createComponent(ContentProjectionTestingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should display the projected content within a span element.', () => {
+    const componentEl = fixture.nativeElement as HTMLElement;
+    const spanEl = componentEl.querySelector('span') as HTMLSpanElement;
+    expect(spanEl.innerHTML).toBe('Test Successful');
   });
 });
