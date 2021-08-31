4e582290c85d43908399ae77f75f2a8205e4edce
Waylon <wayloncn@gmail.com>
2021-08-31 17:14:55 -0400

    added stylings for hover states

--- src/app/components/applied-filters/applied-filters.component.ts
+++ src/app/components/applied-filters/applied-filters.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 import Filter from 'src/app/models/filter.model';
-import { JobListingsService } from 'src/app/services/job-listings.service';
 
 @Component({
   selector: 'app-applied-filters',
