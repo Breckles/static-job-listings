5ef5ba38dbaf8c158be22303088ff453631b1025
Waylon <wayloncn@gmail.com>
2021-08-31 15:47:38 -0400

    completed making the site responsive

--- src/app/components/job-listings/job-listing/job-listing.component.ts
+++ src/app/components/job-listings/job-listing/job-listing.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import JobListing from 'src/app/models/job-listing.model';
 
 @Component({
